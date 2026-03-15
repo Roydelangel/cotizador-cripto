936d074b631af2037ff6cf648266480a6eb96910
Roydelangel <roydelangelaguero@gmail.com>
2026-03-14 23:25:41 -0400

    Mensaje de alerta

--- js/app.js
+++ js/app.js
@@ -58,5 +58,19 @@ function submitFormulario(e) {
 };
 
 function mostrarAlerta(mensaje) {
-      
+    const existeError = document.querySelector('.error');
+
+    if (!existeError) {
+        const divMensaje = document.createElement('DIV');
+        divMensaje.classList.add('error');
+        divMensaje.textContent = mensaje;
+
+        formulario.appendChild(divMensaje);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    };
+
+    
 };
